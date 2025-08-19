fc4932fb38776fe14ebc48261660c60cc042c450
Elias Reimer <83401932+eliasreimer@users.noreply.github.com>
2025-08-19 15:42:20 +0300

    Update systemImproverCRM.js

--- systemImproverCRM.js
+++ systemImproverCRM.js
@@ -5,7 +5,8 @@
         "https://api.github.com/repos/eliasreimer/systemImproverCRM/contents/copyID.js",
         "https://api.github.com/repos/eliasreimer/systemImproverCRM/contents/scenariosInfo.js",      
         "https://api.github.com/repos/eliasreimer/systemImproverCRM/contents/whoExportedIt.js",        
-        "https://api.github.com/repos/eliasreimer/systemImproverCRM/contents/viewingActivity.js"
+        "https://api.github.com/repos/eliasreimer/systemImproverCRM/contents/viewingActivity.js",
+        "https://api.github.com/repos/eliasreimer/systemImproverCRM/contents/nestedScenarios.js"
     ];
 
     // Получение токена
