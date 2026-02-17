898daa78febc20a73d8d5b527163a1e4ea9f65d1
Elias Reimer <83401932+eliasreimer@users.noreply.github.com>
2026-02-17 12:59:27 +0300

    Create managersUIUpdate.js

---
+++ managersUIUpdate.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         S2 CRM. Инструменты и улучшения.
+// @version      2026.02.14
+// @description  ...
+// @author       Илья Раймер <ilya.raymer@skillbox.ru>.
+// @match        https://crm.corp.skillbox.pro/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_notification
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_addStyle
+// @grant        GM_deleteValue
+// @connect      github.com
+// @connect      api.github.com
+// @connect      raw.githubusercontent.com
+// @connect      utsp.corp.skillbox.pro
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/eliasreimer/bootloaders/master/managersUILoader.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
